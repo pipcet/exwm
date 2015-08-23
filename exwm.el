a8825e96a2058c5452ecd1201b2510a576f9e908
Philip <pipcet@gmail.com>
2015-08-23 10:32:17 +0000

    use the new `exwm-layout--set-frame-fullscreen' code

--- exwm.el
+++ exwm.el
@@ -441,39 +441,14 @@
             (props (list (elt data 1) (elt data 2)))
             (buffer (exwm--id->buffer id))
             props-new)
-        (when (not buffer)
+        ;; only support _NET_WM_STATE_FULLSCREEN / _NET_WM_STATE_ADD for frames
+        (when (and (not buffer)
+                   (memq xcb:Atom:_NET_WM_STATE_FULLSCREEN props))
           (dolist (f exwm-workspace--list)
             (when (equal (frame-parameter f 'exwm-outer-id) id)
-              (let ((width (x-display-pixel-width))
-                    (height (x-display-pixel-height)))
-                (xcb:+request exwm--connection
-                    (make-instance 'xcb:ConfigureWindow
-                                   :window id
-                                   :value-mask (logior xcb:ConfigWindow:X
-                                                       xcb:ConfigWindow:Y
-                                                       xcb:ConfigWindow:Width
-                                                       xcb:ConfigWindow:Height)
-                                   :x 0 :y 0
-                                   :width width
-                                   :height height))
-                (xcb:+request exwm--connection
-                    (make-instance 'xcb:SendEvent
-                                   :propagate 0 :destination id
-                                   :event-mask xcb:EventMask:StructureNotify
-                                   :event (xcb:marshal
-                                           (make-instance 'xcb:ConfigureNotify
-                                                          :event id :window id
-                                                          :above-sibling xcb:Window:None
-                                                          :x 0 :y 0
-                                                          :width width :height height
-                                                          :border-width 0
-                                                          :override-redirect 0)
-                                           exwm--connection)))
-            (xcb:+request exwm--connection
-                    (make-instance 'xcb:ewmh:set-_NET_WM_STATE
-                                   :window id
-                                   :data (vector xcb:Atom:_NET_WM_STATE_FULLSCREEN)))
-                (xcb:flush exwm--connection)))))
+              (cond ((= action xcb:ewmh:_NET_WM_STATE_ADD)
+                     (exwm-layout--set-frame-fullscreen f)
+                     (push xcb:Atom:_NET_WM_STATE_FULLSCREEN props-new))))))
         (when buffer                    ;ensure it's managed
           (with-current-buffer buffer
             ;; _NET_WM_STATE_MODAL
